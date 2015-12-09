34c9d6abaf829d200c65bbfa9507ec3190067239
smusings <etyulmenkov@yahoo.com>
2015-12-08 20:41:33 -0500

    random moves finished

--- static/JS/jsonly.js
+++ static/JS/jsonly.js
@@ -14,9 +14,37 @@ XOX.controller('myController', function($scope, $http){
 		{value: '-'}, {value: '-'}, {value: '-'}]
 
 
+	$scope.xCount = 0;
+	$scope.oCount = 0;
 	$scope.move = function(index, value)
 	{
 		$scope.board[index].value = 'X';
+		$scope.xCount++;
+
+		var range = $scope.board.length;
+		if(($scope.xCount + $scope.oCount) < 8)
+		{
+			$scope.aiMove(range);
+		}
+		else
+		{
+			alert("game over!");
+		}
+	}
+
+	$scope.aiMove = function(range)
+	{
+		var move = Math.floor((Math.random() * range) + 1);
+
+		if($scope.board[move] != null && $scope.board[move].value == '-')
+		{
+			$scope.board[move].value = '0'
+			$scope.oCount++;
+		}
+		else
+		{
+			$scope.aiMove(range);
+		}
 	}
 
 	});
